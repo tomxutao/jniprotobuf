3d02c95298e34aa45a49a8db737de41666071dfd
tomyxu <tomyxu@tencent.com>
2022-01-10 10:35:37 +0800

    first commit2

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_PLATFORM     := android-26
+# APP_ABI          := armeabi, armeabi-v7a, arm64-v8a, x86
+APP_ABI          := armeabi-v7a, x86
+APP_STL          := gnustl_static
+APP_MODULES      := protobuf
